ec06755a8033628545b758636d53dfad84f6078c
Lukas Bystricky <lukasby@kth.se>
2020-04-03 16:36:06 +0200

    tests to compare with direct sum implementations

--- PeriodicEwald/tests/direct_sums_test.m
+++ PeriodicEwald/tests/direct_sums_test.m
@@ -7,20 +7,20 @@ clc
 
 initewald
 
-fprintf('*****************************************************\n');
-fprintf('TESTING DIRECT SUMS FOR STOKES SINGLE-LAYER POTENTIAL\n');
-fprintf('*****************************************************\n');
-
 %% Set up data
 Nsrc = 10;
 Ntar = 10;
 
-Lx = 2;
-Ly = 1;
+Lx = 1;
+Ly = 2;
 
 % Two components of the density function
 f1 = 10*rand(Nsrc,1);
 f2 = 10*rand(Nsrc,1);
+
+% Two components of normal vector
+n1 = rand(Nsrc,1);
+n2 = sqrt(1 - n1.^2);
 
 % Source and target loccations
 xsrc = Lx*rand(Nsrc,1);
@@ -29,11 +29,17 @@ ysrc = Ly*rand(Nsrc,1);
 xtar = Lx*rand(Ntar,1);
 ytar = Ly*rand(Ntar,1);
 
-fprintf('CHECKING REAL SUM...\n');
+%% Check single-layer potential
+fprintf('*****************************************************\n');
+fprintf('TESTING DIRECT SUMS FOR STOKES SINGLE-LAYER POTENTIAL\n');
+fprintf('*****************************************************\n');
+
+
 % Compute solution with Spectral Ewald
 [~,~, ur_ewald, uk_ewald, xi] = StokesSLP_ewald_2p(xsrc, ysrc, ...
     xtar, ytar, f1, f2, Lx, Ly, 'verbose', 1);
 
+fprintf('CHECKING REAL SUM...\n');
 % Compute direct sums
 ur_direct = stokes_slp_real_ds(xsrc, ysrc, xtar, ytar,...
                         f1, f2, Lx, Ly, xi);
@@ -48,4 +54,23 @@ uk_direct = stokes_slp_kspace_ds(xsrc, ysrc, xtar, ytar,...
                         f1, f2, Lx, Ly, xi, kinf);
  
 fprintf('MAXIMUM ERROR: %.5e\n',max(max(uk_direct - uk_ewald)));
+
+%% Check double-layer potential
+fprintf('*****************************************************\n');
+fprintf('TESTING DIRECT SUMS FOR STOKES DOUBLE-LAYER POTENTIAL\n');
+fprintf('*****************************************************\n');
+
+% Compute solution with Spectral Ewald
+[~,~, ur_ewald, uk_ewald, xi] = StokesDLP_ewald_2p(xsrc, ysrc, ...
+    xtar, ytar, n1, n2, f1, f2, Lx, Ly, 'verbose', 1);
+
+fprintf('CHECKING REAL SUM...\n');
+% Compute direct sums
+ur_direct = stokes_dlp_real_ds(xsrc, ysrc, xtar, ytar, n1, n2,...
+                        f1, f2, Lx, Ly, xi);
+ 
+fprintf('MAXIMUM ERROR: %.5e\n',max(max(ur_direct - ur_ewald)));
+
+fprintf('*****************************************************\n');
+
 
---
+++ PeriodicEwald/tests/matlab_sums/stokes_dlp_real_ds.m
@@ -0,0 +1,88 @@
+function ureal = stokes_dlp_real_ds(xsrc, ysrc, xtar, ytar,...
+                        n1, n2, f1, f2, Lx, Ly, xi)
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+% Evaluates the real space sum of the Stokeslet directly. Considers one
+% periodic replicate box in each direction.
+%
+% Input:
+%       xsrc, x component of source points
+%       ytar, y component of source points
+%       xsrc, x component of target points
+%       ytar, y component of target points
+%       f1, x component of density function
+%       f2, y component of density function
+%       n1, x component of normal vector at source points
+%       n2, y component of normal vecotr at source points
+%       Lx, the length of the periodic box in the x direction
+%       Ly, the length of the periodic box in the y direction
+%       xi, ewald parameter
+% Output:
+%       ureal, velocity as an 2xN matrix
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+Nsrc = length(xsrc);
+Ntar = size(xtar,1);
+ureal1 = zeros(1, Ntar);
+ureal2 = zeros(1, Ntar);
+
+for n=1:Nsrc
+    
+    for m=1:Ntar 
+        
+        for jpx = -1:1 %Go through a layer of boxes in the x direction
+            
+            for jpy = -1:1 %Go through a layer of boxes in the y direction
+                
+                %Compute periodic source point
+                xsrc_p = xsrc(n) + jpx*Lx;
+                ysrc_p = ysrc(n) + jpy*Ly;
+                
+                r1 = (xsrc_p - xtar(m));
+                r2 = (ysrc_p - ytar(m));
+                r = sqrt(r1^2 + r2^2);
+                               
+                % if r == 0, skip
+                if abs(r) < 1e-13
+                    continue
+                else
+                    [utmp1, utmp2] =  stokeslet_real_sum(r1,r2,...
+                                f1(n),f2(n),n1(n),n2(n),xi);
+                    ureal1(m) = ureal1(m) + utmp1;
+                    ureal2(m) = ureal2(m) + utmp2;
+                end
+            end
+            
+        end
+    end
+end
+
+ureal = [ureal1; ureal2] / (4*pi);
+
+% add on the zero mode from the kspace sum here, because that's done in the
+% real space mex file
+ureal = ureal + zero_mode(xsrc, ysrc, n1, n2, f1, f2) / (Lx*Ly);
+
+end
+
+function [u1, u2] = stokeslet_real_sum(r1, r2, f1, f2, n1, n2, xi)
+
+rdotf = r1*f1 + r2*f2;
+rdotn = r1*n1 + r2*n2;
+ndotf = n1*f1 + n2*f2;
+
+r = sqrt(r1^2 + r2^2);
+
+u1 = exp(-xi^2*r^2)*(4*r1*rdotn*rdotf/r^4 + 2*xi^2*(f1*rdotn + n1*rdotf + ndotf*r1));
+u2 = exp(-xi^2*r^2)*(4*r2*rdotn*rdotf/r^4 + 2*xi^2*(f2*rdotn + n2*rdotf + ndotf*r2));
+
+end
+
+function u0 = zero_mode(xsrc, ysrc, n1, n2, f1, f2)
+
+u0 = zeros(2, length(xsrc));
+
+fdotn = f1.*n1 + f2.*n2;
+u0(1,:) = 4*pi*xsrc.*fdotn;
+u0(2,:) = 4*pi*ysrc.*fdotn;
+
+end
