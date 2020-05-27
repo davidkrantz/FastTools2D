6b7149b398ca1b846d99412d90dc62618907337c
Lukas Bystricky <lukasby@kth.se>
2020-05-27 17:26:14 +0200

    comparison of single-layer gradient Ewald to direct sums

---
+++ PeriodicEwald/tests/matlab_sums/stokes_slp_gradient_kspace_ds.m
@@ -0,0 +1,81 @@
+function ugrad_k = stokes_slp_gradient_kspace_ds(xsrc,ysrc,xtar,ytar,f1,f2,b1,b2,Lx,Ly,xi,kinf)
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+% Evaluates the Fourier space sum of the Stokeslet pressure directly. 
+% Evaluates between -kinf and kinf, and does not use FFTs or fast Gaussian 
+% gridding.
+%
+% Input:
+%       xsrc, x component of source points
+%       ytar, y component of source points
+%       xsrc, x component of target points
+%       ytar, y component of target points
+%       f1, x component of density function
+%       f2, y component of density function
+%       b1, x component of target direction vector
+%       b2, y component of target direction vector
+%       Lx, the length of the periodic box in the x direction
+%       Ly, the length of the periodic box in the y direction
+%       xi, ewald parameter
+%       kinf, truncation length (integer)
+% Output:
+%       pk, pressure as 1xN matrix
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+Nsrc = length(xsrc);
+Ntar = size(xtar,1);
+ugrad_k = zeros(2, Ntar);
+ 
+%Source points
+for n = 1:Nsrc  
+    
+    ugrad_k_tmp = zeros(2,Ntar);
+    
+    %Target points   
+    for m = 1:Ntar      
+        
+        r1 = xtar(m) - xsrc(n);
+        r2 = ytar(m) - ysrc(n);
+        
+        % Loop through Fourier modes in x and y directions
+        for jk1 = -kinf+1:kinf 
+            
+            k1 = 2*pi/Lx*jk1;
+            
+            for jk2 = -kinf+1:kinf
+                k2 = 2*pi/Ly*jk2;
+                
+                if k1 == 0 && k2 == 0
+                    continue
+                end
+                
+                tmp = stokeslet_gradient_k_sum(k1, k2, f1(n), f2(n), b1(m), b2(m), xi);
+                ugrad_k_tmp(:,m) = ugrad_k_tmp(:,m) + tmp*exp(1i*(k1*r1+k2*r2));
+ 
+            end
+        end
+    end
+    
+    ugrad_k = ugrad_k + ugrad_k_tmp;
+    
+end
+
+ugrad_k = real(ugrad_k)/(Lx*Ly);
+
+end
+
+function ugrad_k = stokeslet_gradient_k_sum(k1, k2, f1, f2, b1, b2, xi)
+
+    ugrad_k = [0;0];
+    
+    kdotf = k1*f1 + k2*f2;
+    kdotb = k1*b1 + k2*b2;
+    
+    k = sqrt(k1^2 + k2^2);
+    
+    ugrad_k(1) = 1i*(f1*kdotb/k^2 - k1*kdotb*kdotf/k^4)*(1 + k^2/(4*xi*xi))...
+                    *exp(-k^2/(4*xi*xi));
+    ugrad_k(2) = 1i*(f2*kdotb/k^2 - k2*kdotb*kdotf/k^4)*(1 + k^2/(4*xi*xi))...
+                    *exp(-k^2/(4*xi*xi));            
+end
+
+
---
+++ PeriodicEwald/tests/matlab_sums/stokes_slp_gradient_real_ds.m
@@ -0,0 +1,75 @@
+function ugrad_r = stokes_slp_gradient_real_ds(xsrc, ysrc, xtar, ytar,...
+                        f1, f2, b1, b2, Lx, Ly, xi)
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+% Evaluates the real space sum of the Stokeslet directly. Considers one
+% periodic replicate box in each direction.
+%
+% Input:
+%       xsrc, x component of source points
+%       ytar, y component of source points
+%       xsrc, x component of target points
+%       ytar, y component of target points
+%       f1, x component of density function
+%       f2, y component of density function
+%       b1, x component of target direction vector
+%       b2, y component of target direction vector
+%       Lx, the length of the periodic box in the x direction
+%       Ly, the length of the periodic box in the y direction
+%       xi, ewald parameter
+% Output:
+%       gradient of u, velocity as a 2xN matrix
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+Nsrc = length(xsrc);
+Ntar = size(xtar,1);
+ugrad_r = zeros(2, Ntar);
+
+for n=1:Nsrc
+    
+    for m=1:Ntar 
+        
+        for jpx = -1:1 %Go through a layer of boxes in the x direction
+            
+            for jpy = -1:1 %Go through a layer of boxes in the y direction
+                
+                %Compute periodic source point
+                xsrc_p = xsrc(n) + jpx*Lx;
+                ysrc_p = ysrc(n) + jpy*Ly;
+                
+                r1 = (xsrc_p - xtar(m));
+                r2 = (ysrc_p - ytar(m));
+                r = sqrt(r1^2 + r2^2);
+                               
+                if abs(r) < 1e-13
+                    continue
+                else
+                    ugrad_tmp =  stokeslet_gradient_real_sum(r1,r2,f1(n),f2(n),b1(m),b2(m),xi);
+                    ugrad_r(:,m) = ugrad_r(:,m) + ugrad_tmp;
+                end
+            end
+            
+        end
+    end
+end
+
+ugrad_r = ugrad_r / (4*pi);
+
+end
+
+function ugrad_real = stokeslet_gradient_real_sum(r1, r2, f1, f2, b1, b2, xi)
+
+ugrad_real = [0;0];
+
+rdotf = r1*f1 + r2*f2;
+rdotb = r1*b1 + r2*b2;
+bdotf = f1*b1 + f2*b2;
+
+r = sqrt(r1^2 + r2^2);
+
+ugrad_real(1) = exp(-xi*xi*r^2)*(2*xi*xi*rdotb*f1 - 2*xi*xi*rdotb*rdotf*r1/r^2 ...
+        + (rdotf + bdotf*r1 - f1*rdotb)/r^2 - 2*r1*rdotb*rdotf/r^4);
+
+ugrad_real(2) = exp(-xi*xi*r^2)*(2*xi*xi*rdotb*f2 - 2*xi*xi*rdotb*rdotf*r2/r^2 ...
+        + (rdotf + bdotf*r2 - f2*rdotb)/r^2 - 2*r2*rdotb*rdotf/r^4);    
+
+end
