458659a27d0f3fb80769f26ac99660cee56ea05e
Lukas Bystricky <lukasby@kth.se>
2020-05-28 16:24:25 +0200

    consistency check for the pressure

---
+++ PeriodicEwald/tests/consistency_checks/consistency_test_pressure.m
@@ -0,0 +1,83 @@
+% This is a test script to check the consistency of the Ewald method. In
+% particular we look at what happens when we change the Ewald parameters,
+% and what happens when we replicate the reference cell. 
+
+close all
+clearvars
+clc
+
+initewald
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Single-layer potential 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fprintf("\n*********************************************************\n");
+fprintf('Checking consistency of single-layer potential...\n');
+fprintf("*********************************************************\n\n");
+
+%% Set up data
+Nsrc = 10000;
+Ntar = 10000;
+
+Lx = 2*pi;
+Ly = 2*pi;
+
+% Two components of the density function
+f1 = 10*rand(Nsrc,1);
+f2 = 10*rand(Nsrc,1);
+
+% Source and target loccations
+xsrc = Lx*rand(Nsrc,1);
+ysrc = Ly*rand(Nsrc,1);
+
+xtar = Lx*rand(Ntar,1);
+ytar = Ly*rand(Ntar,1);
+
+%% Compute solution with Spectral Ewald
+% Here we change Nb, the number of points in each box in the real space
+% sum. This controls the cutoff radius in real space, and that in turn
+% changes the Ewald parameter xi, and the cutoff mode in Fourier space. 
+% Changing Nb should not change the result, but it will have an impact on
+% the relative times to compute the real space and Fourier sums. This
+% parameter can be studied to find an optimum balance.
+
+Nb = [3, 9, 27];
+
+p = zeros(Ntar, length(Nb));
+
+for j = 1:length(Nb)
+    tic
+    p_tmp = StokesSLP_pressure_ewald_2p(xsrc, ysrc, xtar, ytar, f1, f2, Lx, Ly,...
+                'Nb', Nb(j), 'verbose', 1);
+    fprintf('Nb %d: Spectral Ewald (mex) computed in %.5f s\n', Nb(j), toc);
+    
+    p(:,j) = p_tmp;
+end
+
+%% Check that using a different number of bins doesn't affect solution
+
+E1 = zeros(length(Nb), length(Nb));
+for j = 1:length(Nb)
+    for i = 1:length(Nb)
+        E1(i,j) = max(abs(p(:,i) - p(:,j)));
+    end
+end
+fprintf('\nMaximum error from changing number of bins for SLP: %.5e\n',...
+    max(max(E1)));
+
+%% Check that replicating boxes doesn't affect solution
+
+p1 = StokesSLP_pressure_ewald_2p(xsrc, ysrc, xtar, ytar, f1, f2, Lx, Ly);
+
+xsrc = [xsrc; xsrc + Lx];
+ysrc = [ysrc; ysrc];
+f1 = [f1; f1];
+f2 = [f2; f2];
+
+Lx = 2*Lx;
+
+p2 = StokesSLP_pressure_ewald_2p(xsrc, ysrc, xtar, ytar, f1, f2, Lx, Ly);
+
+fprintf('\nMaximum error from creating periodic replicate for SLP: %.5e\n',...
+    max(abs(p1 - p2)));
+
