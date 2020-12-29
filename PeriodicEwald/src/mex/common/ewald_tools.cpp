a8f3769610af6a7870ff50e066205ca8fd71df0b
Lukas Bystricky <lukasby@kth.se>
2020-12-29 16:17:10 +0100

    added spreading and gathering functions

--- PeriodicEwald/src/mex/common/ewald_tools.cpp
+++ PeriodicEwald/src/mex/common/ewald_tools.cpp
@@ -4,7 +4,7 @@
  *This function assigns particles to boxes on the current grid.
  *------------------------------------------------------------------------
  */
-void Assign(double *psrc, double *ptar, double Lx, double Ly, int nsrc, 
+void Assign(double* psrc, double* ptar, double Lx, double Ly, int nsrc,
         int ntar, int nside_x, int nside_y, int* particle_offsets_src,
         int* box_offsets_src,int* nsources_in_box, int* particle_offsets_tar,
         int* box_offsets_tar,int* ntargets_in_box){
@@ -66,7 +66,7 @@ void Assign(double *psrc, double *ptar, double Lx, double Ly, int nsrc,
         
         box_offsets_tar[j] = itar;
     }
-       
+    
     //The offsets of the particles in each box in the z and q arrays.
     for(j=0;j<nsrc;j++)
         particle_offsets_src[particlenum_in_box_src[in_box_src[j]]++] = j;
@@ -76,39 +76,227 @@ void Assign(double *psrc, double *ptar, double Lx, double Ly, int nsrc,
     delete in_box_src;
     delete particlenum_in_box_src;
     delete in_box_tar;
-    delete particlenum_in_box_tar;   
+    delete particlenum_in_box_tar;
 }
 
 /*------------------------------------------------------------------------
  *This function finds the node to begin the Gaussian blur
  *------------------------------------------------------------------------
  */
-void FindClosestNode(double x, double y, double Lx, double Ly, double h, int P, 
-			int* mx, int* my, double* px, double* py){
-	
-	double TOL = 1e-13;
+void FindClosestNode(double x, double y, double Lx, double Ly, double h, int P,
+        int* mx, int* my, double* px, double* py){
+    
+    double TOL = 1e-13;
+    
+    *px = x - h*floor(x/h);
+    *py = y - h*floor(y/h);
+    
+    double Lhalf_x = static_cast<double>(Lx/2.0);
+    double Lhalf_y = static_cast<double>(Ly/2.0);
+    
+    //(mx,my) is the center grid point in H1 and H2.
+    *mx = static_cast<int>(floor((x+Lhalf_x)/h) - P/2);
+    *my = static_cast<int>(floor((y+Lhalf_y)/h) - P/2);
+    
+    // correct for cases where target is very close to a grid node
+    
+    if (fabs(*px) < TOL || fabs(*px - h) < TOL)
+        *px = 0;
+    
+    if (fabs(*py) < TOL || fabs(*py - h) < TOL)
+        *py = 0;
+    
+    if (*px == 0 && remainder((x+Lhalf_x)/h - P/2, 1) < 0)
+        (*mx)++;
+    
+    if (*py == 0 && remainder((y+Lhalf_y)/h - P/2, 1) < 0)
+        (*my)++;
+}
 
-        *px = x - h*floor(x/h);
-        *py = y - h*floor(y/h);
-
-        double Lhalf_x = static_cast<double>(Lx/2.0);
-        double Lhalf_y = static_cast<double>(Ly/2.0);
+/*------------------------------------------------------------------------
+ *This function speads a vector field to a uniform grid
+ *------------------------------------------------------------------------
+ */
+void Spread(double* H1, double* H2, double* e1, double* psrc, double* f,
+        int Nsrc, double Lx, double Ly, double xi, double w,
+        double eta, int P, int Mx, int My, double h){
+    
+    //This is the precomputable part of the fast Gaussian gridding.
+    double tmp = -2*xi*xi/eta*h*h;
+    for(int j = -P/2;j<=P/2;j++)
+        e1[j+P/2] = exp(tmp*j*j);
+    
+    //Spreading the sources to the grid is not a completely parallel
+    //operation. We use the simple approach of locking the column of the
+    //matrix we are working on currently. This might not be optimal but it
+    //is simple to implement.
+    omp_lock_t* locks = new omp_lock_t[Mx];
+    for(int j = 0;j<Mx;j++)
+        omp_init_lock(&locks[j]);
+    
+    //We use OpenMP for simple parallelization.
+#pragma omp parallel for
+    for(int k = 0;k<Nsrc;k++) {
         
-        //(mx,my) is the center grid point in H1 and H2.
-        *mx = static_cast<int>(floor((x+Lhalf_x)/h) - P/2);
-        *my = static_cast<int>(floor((y+Lhalf_y)/h) - P/2);
-
-	// correct for cases where target is very close to a grid node
-
-	if (fabs(*px) < TOL || fabs(*px - h) < TOL)
-            *px = 0;
+        //The Gaussian bells are translation invariant. We exploit this
+        //fact to avoid blow-up of the terms and the numerical instability
+        //that follows. (px,py) is the center of the bell with the original
+        //grid-alignment but close to the origin.
         
-        if (fabs(*py) < TOL || fabs(*py - h) < TOL)
-            *py = 0;
+        double xsrc = psrc[2*k];
+        double ysrc = psrc[2*k+1];
+        
+        int mx, my;
+        double px, py;
+        
+        FindClosestNode(xsrc, ysrc, Lx, Ly, h, P, &mx, &my, &px, &py);
+        
+        //Some auxillary quantities for the fast Gaussian gridding.
+        double tmp = -2*xi*xi/eta;
+        double ex = exp(tmp*(px*px+py*py + 2*w*px));
+        double e4y = exp(2*tmp*w*py);
+        double e3x = exp(-2*tmp*h*px);
+        double e3y = exp(-2*tmp*h*py);
+        
+        //We add the Gaussians column by column, and lock the one we are
+        //working on to avoid race conditions.
+        for(int x = 0;x<P+1;x++) {
+            double ey = ex*e4y*e1[x];
+            int xidx = ((x+mx+Mx)%Mx)*My;
+            
+            omp_set_lock(&locks[(x+mx+Mx)%Mx]);
+            if(my >= 0 && my < My-P-1) {
+                int idx = my+xidx;
+                for(int y = 0;y<P+1;y++,idx++) {
+                    double tmp = ey*e1[y];
+                    
+                    H1[idx] += tmp*f[2*k];
+                    H2[idx] += tmp*f[2*k+1];
+                    ey *= e3y;
+                }
+            }
+            else{
+                for(int y = 0;y<P+1;y++) {
+                    double tmp = ey*e1[y];
+                    int idx = ((y+my+My)%My)+xidx;
+                    
+                    H1[idx] += tmp*f[2*k];
+                    H2[idx] += tmp*f[2*k+1];
+                    ey *= e3y;
+                }
+            }
+            omp_unset_lock(&locks[(x+mx+Mx)%Mx]);
+            ex *= e3x;
+        }
+    }
+    
+    //Spreading is the only part of the k-space sum that needs locks, so
+    //get rid of them.
+    for(int j = 0;j<Mx;j++)
+        omp_destroy_lock(&locks[j]);
+    
+    delete locks;
+}
 
-	if (*px == 0 && remainder((x+Lhalf_x)/h - P/2, 1) < 0)
-		(*mx)++;
+/*------------------------------------------------------------------------
+ *This function performs the evaluation step, gathering the data at the 
+ *target points
+ *------------------------------------------------------------------------
+ */
+void Gather(double* H, int total_components, int component_number, 
+        double* e1, double* ptar, double* output,
+        int Ntar, double Lx, double Ly, double xi, double w,
+        double eta, int P, int Mx, int My, double h){
+    
+#pragma omp parallel for
+    for(int k = 0;k<Ntar;k++) {
+        
+        int target_index = total_components*k + component_number-1;
+        //The Gaussian bells are translation invariant. We exploit this
+        //fact to avoid blow-up of the terms and the numerical instability
+        //that follows. (px,py) is the center of the bell with the original
+        //grid-alignment but close to the origin.
+        
+        double xtar = ptar[2*k];
+        double ytar = ptar[2*k+1];
+        
+        int mx, my;
+        double px, py;
+        
+        FindClosestNode(xtar, ytar, Lx, Ly, h, P, &mx, &my, &px, &py);
+        
+        double tmp = -2*xi*xi/eta;
+        double ex = exp(tmp*(px*px+py*py + 2*w*px));
+        double e4y = exp(2*tmp*w*py);
+        double e3x = exp(-2*tmp*h*px);
+        double e3y = exp(-2*tmp*h*py);
+        
+        //If there is no wrap-around due to periodicity for this gaussian,
+        //we use a faster loop. We go column by column, but as the
+        //matrices Ht1 and Ht2 are only read from we have no need for
+        //locks and such.
+        if(mx >= 0 && my >= 0 && mx < Mx-P-1 && my < My-P-1) {
+            int idx = mx*My+my;
+            for(int x = 0;x<P+1;x++) {
+                double ey = ex*e4y*e1[x];
+                
+                for(int y = 0;y<P+1;y++) {
+                    double tmp = ey*e1[y];
+                    output[target_index] += tmp*H[idx];
+                    idx++;
+                    ey *= e3y;
+                }
+                ex *= e3x;
+                idx += My-P-1;
+            }
+        }
+        else{
+            for(int x = 0;x<P+1;x++) {
+                double ey = ex*e4y*e1[x];
+                int xidx = ((x+mx+Mx)%Mx)*My;
+                for(int y = 0;y<P+1;y++) {
+                    double tmp = ey*e1[y];
+                    int idx = ((y+my+My)%My)+xidx;
+                    output[target_index] += tmp*H[idx];
+                    ey *= e3y;
+                }
+                ex *= e3x;
+            }
+        }
+        
+        tmp = 4*xi*xi/eta;
+        tmp = tmp*tmp*h*h/pi;
+        output[target_index] *= tmp / (4*pi);
+    }
+}
 
-	if (*py == 0 && remainder((y+Lhalf_y)/h - P/2, 1) < 0)
-		(*my)++;
+/*------------------------------------------------------------------------
+ *This function extracts the real and imaginary parts of an FFT
+ *------------------------------------------------------------------------
+ */
+void ExtractRealIm(mxArray *fftvector, double *Hhat_re, double *Hhat_im,
+        int Mx, int My)
+{
+    
+    mexPrintf("1\n");
+    
+    //The output of the FFT is complex. Get pointers to the real and
+    //imaginary parts of Hhat1 and Hhat2.
+    Hhat_re = mxGetPr(fftvector);
+    Hhat_im = mxGetPi(fftvector);
+    
+    mwSize cs = Mx*My;
+    
+    mexPrintf("2\n");
+    
+    //We cannot assume that both the real and imaginary part of the
+    //Fourier transforms are non-zero.
+    if(Hhat_im == NULL) {
+        Hhat_im = (double*) mxCalloc(cs,sizeof(double));
+        mxSetPi(fftvector,Hhat_im);
+    }
+    if(Hhat_re == NULL) {
+        Hhat_re = (double*) mxCalloc(cs,sizeof(double));
+        mxSetPr(fftvector,Hhat_re);
+    }
 }
--- PeriodicEwald/src/mex/common/ewald_tools.h
+++ PeriodicEwald/src/mex/common/ewald_tools.h
@@ -3,7 +3,10 @@
 
 #include <math.h>
 #include <string.h>
+#include <omp.h>
 #include "mex.h"
+
+#define pi 3.1415926535897932385
 
 void Assign(double *psrc, double *ptar, double len_x, double len_y, int nsrc, 
         int ntar, int nside_x, int nside_y, int* particle_offsets_src,
@@ -12,4 +15,16 @@ void Assign(double *psrc, double *ptar, double len_x, double len_y, int nsrc,
 
 void FindClosestNode(double x, double y, double Lx, double Ly, double h, int P, 
 			int* mx, int* my, double* px, double* py);
+
+void Spread(double* H1, double* H2, double* e1, double* psrc, double* f, 
+                int Nsrc, double Lx, double Ly, double xi, double w,
+                double eta, int P, int Mx, int My, double h);
+
+void Gather(double* H, int total_components, int component_number, 
+        double* e1, double* ptar, double* output,
+        int Ntar, double Lx, double Ly, double xi, double w,
+        double eta, int P, int Mx, int My, double h);
+        
+void ExtractRealIm(mxArray *fftvector, double *Hhat_re, double *Hhat_im, 
+        int Mx, int My);
 #endif
